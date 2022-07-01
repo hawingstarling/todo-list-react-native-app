e7a7fab08676f8421945fb0da3ed155ef7613769
todo-list <dinhthanhtien2k2@gmail.com>
2022-07-02 01:52:46 +0700

    Initialize todo list

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, TextInput, Platform, StyleSheet, Text, View, TouchableOpacity, Keyboard } from 'react-native';
+import Task from './component/Task'
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss()
+    setTaskItems([...taskItems, task])
+    setTask('')
+  }
+
+  const completeTask = (index) => {
+    // coppy array
+    // won't it re-render, because set taskItems == data address 
+    // shadow comparison
+    let itemsCoppy = [...taskItems]
+    itemsCoppy.splice(index, 1)
+    setTaskItems(itemsCoppy)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          {taskItems.map((task, index) => (
+            <TouchableOpacity onPress={() => completeTask(index)}>
+              <Task key={index} text={task} />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'android' ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput value={task} onChangeText={text => setTask(text)} style={styles.input} placeholder={'Write a task'} /> 
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +52,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED'
   },
-});
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    width: 250,
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addText: {
+
+  },
+})
